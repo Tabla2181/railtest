2667bcb05c22a63b7ade3a9f3daa5bd9853961dd
Tabla2181 <102341979+Tabla2181@users.noreply.github.com>
2022-12-04 12:40:36 -0500

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ API_HASH = os.environ.get('api_hash','c2cbb2f07c44fe466076fbe65e3c00cc')
 SPLIT_FILE = 1024 * 1024 * int(os.environ.get('split_file','99'))
 ROOT_PATH = 'root/'
 ACCES_USERS = os.environ.get('tl_admin_user','Elnietodecacha').split(';')
-PROXY = ProxyCloud.parse(os.environ.get('proxy_enc','http://KADKFLCAJDJJKJJGDYFASDDJHNJGDADKDGLGDELI'))
+PROXY = ProxyCloud.parse(os.environ.get('proxy_enc','http://198.59.191.234:8080'))
 
 if PROXY:
   print(f'Proxy {PROXY.as_dict_proxy()}')
